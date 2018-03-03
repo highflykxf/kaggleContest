60f24775da231aedcac84fa1fa582513237dd7dc
highfly <xiangfei0919@gmail.com>
2018-03-03 21:53:01 +0800

    add exercises&Titanic

--- Titanic/main_1.py
+++ Titanic/main_1.py
@@ -151,3 +151,366 @@ class SklearnHelper(object):
         print(self.clf.fit(x,y).feature_importances_)
     
 # Class to extend XGboost classifer
+
+# Out-of-Fold Predictions
+def get_oof(clf, x_train, y_train, x_test):
+    oof_train = np.zeros((ntrain,))
+    oof_test = np.zeros((ntest,))
+    oof_test_skf = np.empty((NFOLDS, ntest))
+
+    for i, (train_index, test_index) in enumerate(kf):
+        x_tr = x_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = x_train[test_index]
+
+        clf.train(x_tr, y_tr)
+
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(x_test)
+
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+# Generating base first-level models
+# Put in our parameters for said classifiers
+# Just a quick summary of the parameters that we will be listing here for completeness,
+
+# **n_jobs** : Number of cores used for the training process. If set to -1, all cores are used.
+
+# **n_estimators** : Number of classification trees in your learning model ( set to 10 per default)
+
+# **max_depth** : Maximum depth of tree, or how much a node should be expanded. Beware if set to too high  a number would run the risk of overfitting as one would be growing the tree too deep
+
+# **verbose** : Controls whether you want to output any text during the learning process. A value of 0 suppresses all text while a value of 3 outputs the tree learning process at every iteration.
+# Random Forest parameters
+rf_params = {
+    'n_jobs': -1,
+    'n_estimators': 500,
+     'warm_start': True, 
+     #'max_features': 0.2,
+    'max_depth': 6,
+    'min_samples_leaf': 2,
+    'max_features' : 'sqrt',
+    'verbose': 0
+}
+
+# Extra Trees Parameters
+et_params = {
+    'n_jobs': -1,
+    'n_estimators':500,
+    #'max_features': 0.5,
+    'max_depth': 8,
+    'min_samples_leaf': 2,
+    'verbose': 0
+}
+
+# AdaBoost parameters
+ada_params = {
+    'n_estimators': 500,
+    'learning_rate' : 0.75
+}
+
+# Gradient Boosting parameters
+gb_params = {
+    'n_estimators': 500,
+     #'max_features': 0.2,
+    'max_depth': 5,
+    'min_samples_leaf': 2,
+    'verbose': 0
+}
+
+# Support Vector Classifier parameters 
+svc_params = {
+    'kernel' : 'linear',
+    'C' : 0.025
+    }
+
+# Create 5 objects that represent our 4 models
+rf = SklearnHelper(clf=RandomForestClassifier, seed=SEED, params=rf_params)
+et = SklearnHelper(clf=ExtraTreesClassifier, seed=SEED, params=et_params)
+ada = SklearnHelper(clf=AdaBoostClassifier, seed=SEED, params=ada_params)
+gb = SklearnHelper(clf=GradientBoostingClassifier, seed=SEED, params=gb_params)
+svc = SklearnHelper(clf=SVC, seed=SEED, params=svc_params)
+
+# Creating Numpy arrays out of our train and test sets
+# Create Numpy arrays of train, test and target ( Survived) dataframes to feed into our models
+y_train = train['Survived'].ravel()
+train = train.drop(['Survived'], axis=1)
+x_train = train.values # Creates an array of the train data
+x_test = test.values # Creats an array of the test data
+
+# Output of the first level predictions
+# Create our OOF train and test predictions. These base results will be used as new features
+et_oof_train, et_oof_test = get_oof(et, x_train, y_train, x_test) # Extra Trees
+rf_oof_train, rf_oof_test = get_oof(rf,x_train, y_train, x_test) # Random Forest
+ada_oof_train, ada_oof_test = get_oof(ada, x_train, y_train, x_test) # AdaBoost 
+gb_oof_train, gb_oof_test = get_oof(gb,x_train, y_train, x_test) # Gradient Boost
+svc_oof_train, svc_oof_test = get_oof(svc,x_train, y_train, x_test) # Support Vector Classifier
+
+print("Training is complete")
+
+# Features importances generated from the different classifiers
+rf_feature = rf.feature_importances(x_train,y_train)
+et_feature = et.feature_importances(x_train, y_train)
+ada_feature = ada.feature_importances(x_train, y_train)
+gb_feature = gb.feature_importances(x_train,y_train)
+
+# features importances output
+# rf_features = [0.10474135,  0.21837029,  0.04432652,  0.02249159,  0.05432591,  0.02854371
+#   ,0.07570305,  0.01088129 , 0.24247496,  0.13685733 , 0.06128402]
+# et_features = [ 0.12165657,  0.37098307  ,0.03129623 , 0.01591611 , 0.05525811 , 0.028157
+#   ,0.04589793 , 0.02030357 , 0.17289562 , 0.04853517,  0.08910063]
+# ada_features = [0.028 ,   0.008  ,      0.012   ,     0.05866667,   0.032 ,       0.008
+#   ,0.04666667 ,  0.     ,      0.05733333,   0.73866667,   0.01066667]
+# gb_features = [ 0.06796144 , 0.03889349 , 0.07237845 , 0.02628645 , 0.11194395,  0.04778854
+#   ,0.05965792 , 0.02774745,  0.07462718,  0.4593142 ,  0.01340093]
+
+cols = train.columns.values
+# Create a dataframe with features
+feature_dataframe = pd.DataFrame( {'features': cols,
+     'Random Forest feature importances': rf_features,
+     'Extra Trees  feature importances': et_features,
+      'AdaBoost feature importances': ada_features,
+    'Gradient Boost feature importances': gb_features
+    })
+
+# Interactive feature importances via Plotly scatterplots
+# Scatter plot 
+trace = go.Scatter(
+    y = feature_dataframe['Random Forest feature importances'].values,
+    x = feature_dataframe['features'].values,
+    mode='markers',
+    marker=dict(
+        sizemode = 'diameter',
+        sizeref = 1,
+        size = 25,
+#       size= feature_dataframe['AdaBoost feature importances'].values,
+        #color = np.random.randn(500), #set color equal to a variable
+        color = feature_dataframe['Random Forest feature importances'].values,
+        colorscale='Portland',
+        showscale=True
+    ),
+    text = feature_dataframe['features'].values
+)
+data = [trace]
+
+layout= go.Layout(
+    autosize= True,
+    title= 'Random Forest Feature Importance',
+    hovermode= 'closest',
+#     xaxis= dict(
+#         title= 'Pop',
+#         ticklen= 5,
+#         zeroline= False,
+#         gridwidth= 2,
+#     ),
+    yaxis=dict(
+        title= 'Feature Importance',
+        ticklen= 5,
+        gridwidth= 2
+    ),
+    showlegend= False
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig,filename='scatter2010')
+
+# Scatter plot 
+trace = go.Scatter(
+    y = feature_dataframe['Extra Trees  feature importances'].values,
+    x = feature_dataframe['features'].values,
+    mode='markers',
+    marker=dict(
+        sizemode = 'diameter',
+        sizeref = 1,
+        size = 25,
+#       size= feature_dataframe['AdaBoost feature importances'].values,
+        #color = np.random.randn(500), #set color equal to a variable
+        color = feature_dataframe['Extra Trees  feature importances'].values,
+        colorscale='Portland',
+        showscale=True
+    ),
+    text = feature_dataframe['features'].values
+)
+data = [trace]
+
+layout= go.Layout(
+    autosize= True,
+    title= 'Extra Trees Feature Importance',
+    hovermode= 'closest',
+#     xaxis= dict(
+#         title= 'Pop',
+#         ticklen= 5,
+#         zeroline= False,
+#         gridwidth= 2,
+#     ),
+    yaxis=dict(
+        title= 'Feature Importance',
+        ticklen= 5,
+        gridwidth= 2
+    ),
+    showlegend= False
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig,filename='scatter2010')
+
+# Scatter plot 
+trace = go.Scatter(
+    y = feature_dataframe['AdaBoost feature importances'].values,
+    x = feature_dataframe['features'].values,
+    mode='markers',
+    marker=dict(
+        sizemode = 'diameter',
+        sizeref = 1,
+        size = 25,
+#       size= feature_dataframe['AdaBoost feature importances'].values,
+        #color = np.random.randn(500), #set color equal to a variable
+        color = feature_dataframe['AdaBoost feature importances'].values,
+        colorscale='Portland',
+        showscale=True
+    ),
+    text = feature_dataframe['features'].values
+)
+data = [trace]
+
+layout= go.Layout(
+    autosize= True,
+    title= 'AdaBoost Feature Importance',
+    hovermode= 'closest',
+#     xaxis= dict(
+#         title= 'Pop',
+#         ticklen= 5,
+#         zeroline= False,
+#         gridwidth= 2,
+#     ),
+    yaxis=dict(
+        title= 'Feature Importance',
+        ticklen= 5,
+        gridwidth= 2
+    ),
+    showlegend= False
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig,filename='scatter2010')
+
+# Scatter plot 
+trace = go.Scatter(
+    y = feature_dataframe['Gradient Boost feature importances'].values,
+    x = feature_dataframe['features'].values,
+    mode='markers',
+    marker=dict(
+        sizemode = 'diameter',
+        sizeref = 1,
+        size = 25,
+#       size= feature_dataframe['AdaBoost feature importances'].values,
+        #color = np.random.randn(500), #set color equal to a variable
+        color = feature_dataframe['Gradient Boost feature importances'].values,
+        colorscale='Portland',
+        showscale=True
+    ),
+    text = feature_dataframe['features'].values
+)
+data = [trace]
+
+layout= go.Layout(
+    autosize= True,
+    title= 'Gradient Boosting Feature Importance',
+    hovermode= 'closest',
+#     xaxis= dict(
+#         title= 'Pop',
+#         ticklen= 5,
+#         zeroline= False,
+#         gridwidth= 2,
+#     ),
+    yaxis=dict(
+        title= 'Feature Importance',
+        ticklen= 5,
+        gridwidth= 2
+    ),
+    showlegend= False
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig,filename='scatter2010')
+
+# Create the new column containing the average of values
+
+feature_dataframe['mean'] = feature_dataframe.mean(axis= 1) # axis = 1 computes the mean row-wise
+feature_dataframe.head(3)
+
+y = feature_dataframe['mean'].values
+x = feature_dataframe['features'].values
+data = [go.Bar(
+            x= x,
+             y= y,
+            width = 0.5,
+            marker=dict(
+               color = feature_dataframe['mean'].values,
+            colorscale='Portland',
+            showscale=True,
+            reversescale = False
+            ),
+            opacity=0.6
+        )]
+
+layout= go.Layout(
+    autosize= True,
+    title= 'Barplots of Mean Feature Importance',
+    hovermode= 'closest',
+#     xaxis= dict(
+#         title= 'Pop',
+#         ticklen= 5,
+#         zeroline= False,
+#         gridwidth= 2,
+#     ),
+    yaxis=dict(
+        title= 'Feature Importance',
+        ticklen= 5,
+        gridwidth= 2
+    ),
+    showlegend= False
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='bar-direct-labels')
+
+# First-level output as new features
+base_predictions_train = pd.DataFrame( {'RandomForest': rf_oof_train.ravel(),
+     'ExtraTrees': et_oof_train.ravel(),
+     'AdaBoost': ada_oof_train.ravel(),
+      'GradientBoost': gb_oof_train.ravel()
+    })
+base_predictions_train.head()
+
+# Correlation Heatmap of the Second Level Training set
+data = [
+    go.Heatmap(
+        z= base_predictions_train.astype(float).corr().values ,
+        x=base_predictions_train.columns.values,
+        y= base_predictions_train.columns.values,
+          colorscale='Viridis',
+            showscale=True,
+            reversescale = True
+    )
+]
+py.iplot(data, filename='labelled-heatmap')
+
+x_train = np.concatenate(( et_oof_train, rf_oof_train, ada_oof_train, gb_oof_train, svc_oof_train), axis=1)
+x_test = np.concatenate(( et_oof_test, rf_oof_test, ada_oof_test, gb_oof_test, svc_oof_test), axis=1)
+
+# second level learning model via XGBoost
+gbm = xgb.XGBClassifier(
+    #learning_rate = 0.02,
+ n_estimators= 2000,
+ max_depth= 4,
+ min_child_weight= 2,
+ #gamma=1,
+ gamma=0.9,                        
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'binary:logistic',
+ nthread= -1,
+ scale_pos_weight=1).fit(x_train, y_train)
+predictions = gbm.predict(x_test)
+
+# Generate Submission File 
+StackingSubmission = pd.DataFrame({ 'PassengerId': PassengerId,
+                            'Survived': predictions })
+StackingSubmission.to_csv("StackingSubmission.csv", index=False)
